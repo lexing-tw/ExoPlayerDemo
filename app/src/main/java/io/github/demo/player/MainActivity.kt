37c3a0a8effb08953b265412ea877c4fc2a28365
Le Xing <Le.Xing@foxtel.com.au>
2022-04-07 16:59:32 +0800

    [Le] Basic playback

--- app/src/main/java/io/github/demo/player/MainActivity.kt
+++ app/src/main/java/io/github/demo/player/MainActivity.kt
@@ -1,11 +1,41 @@
 package io.github.demo.player
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.OptIn
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import io.github.demo.player.databinding.ActivityMainBinding
 
+@OptIn(UnstableApi::class)
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var exoPlayer: ExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        exoPlayer = ExoPlayer.Builder(this).build().apply {
+            playWhenReady = true
+        }
+        binding.playerView.player = exoPlayer
+        playSample()
+    }
+
+    private fun playSample() {
+        val sampleVideo =
+            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        val mediaItem = MediaItem.fromUri(sampleVideo)
+        exoPlayer?.setMediaItem(mediaItem)
+        exoPlayer?.prepare()
+    }
+
+    override fun onDestroy() {
+        exoPlayer?.release()
+        exoPlayer = null
+        super.onDestroy()
     }
 }
